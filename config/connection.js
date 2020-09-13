9683ad07b70c2adb84d899405f91adf2ca5b643b
digilau <rickyfukfuk@gmail.com>
2020-09-13 16:23:40 -0700

    change the connection

--- config/connection.js
+++ config/connection.js
@@ -5,14 +5,15 @@ var connection;
 
 if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else{
+} else {
 	connection = mysql.createConnection({
-	host: 'localhost',
-	port: 3306,
-	user: 'root',
-	password: 'password',
-	database: 'burgers_db',
-});
+		host: 'localhost',
+		port: 3306,
+		user: 'root',
+		password: 'password',
+		database: 'burgers_db',
+	});
+}
 
 // Make connection.
 connection.connect(function (err) {
--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,7 @@
+use mk0n995u09yeo78v;
+
+DROP TABLE if exists burgers;
+
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
@@ -6,5 +10,6 @@ CREATE TABLE burgers
 	id int NOT NULL AUTO_INCREMENT,
 	burger_name varchar(255) NOT NULL,
 	done BOOLEAN DEFAULT false,
+	createdAt TIMESTAMP NOT NULL,
 	PRIMARY KEY (id)
 );
